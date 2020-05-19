c8df237ea9cd93edae433bcf3a2875506e94814c
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2020-05-19 23:33:36 +1000

    Fix incorrect alloc arguments

--- openjdk/mmtkMutator.cpp
+++ openjdk/mmtkMutator.cpp
@@ -9,11 +9,11 @@ MMTkMutatorContext MMTkMutatorContext::bind(::Thread* current) {
 HeapWord* MMTkMutatorContext::alloc(size_t bytes, Allocator allocator) {
     // FIXME: Proper use of slow-path api
 #ifdef MMTK_GC_NOGC
-    HeapWord* o = (HeapWord*) ::alloc((MMTk_Mutator) this, bytes, HeapWordSize, 0);
+    HeapWord* o = (HeapWord*) ::alloc((MMTk_Mutator) this, bytes, HeapWordSize, 0, allocator);
     // post_alloc((MMTk_Mutator) this, o, NULL, bytes, a);
     return o;
 #elif MMTK_GC_SEMISPACE
-    HeapWord* o = (HeapWord*) ::alloc((MMTk_Mutator) this, bytes, HeapWordSize, 0);
+    HeapWord* o = (HeapWord*) ::alloc((MMTk_Mutator) this, bytes, HeapWordSize, 0, allocator);
     // post_alloc((MMTk_Mutator) this, o, NULL, bytes, a);
     return o;
 #else
