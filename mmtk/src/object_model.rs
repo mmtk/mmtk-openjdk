22d0ad436b20c046b00387be8185e9acdb8d8e94
Javad Amiri <javad.amiri@anu.edu.au>
2020-11-27 14:54:17 +1100

    make objectmodel consistent

--- mmtk/src/object_model.rs
+++ mmtk/src/object_model.rs
@@ -44,89 +44,12 @@ impl ObjectModel<OpenJDK> for VMObjectModel {
         unimplemented!()
     }
 
-    fn get_size_when_copied(_object: ObjectReference) -> usize {
-        unimplemented!()
-    }
-
-    fn get_align_when_copied(_object: ObjectReference) -> usize {
-        unimplemented!()
-    }
-
-    fn get_align_offset_when_copied(_object: ObjectReference) -> isize {
-        unimplemented!()
-    }
-
     fn get_current_size(object: ObjectReference) -> usize {
         unsafe { ((*UPCALLS).get_object_size)(object) }
     }
 
-    fn get_next_object(_object: ObjectReference) -> ObjectReference {
-        unimplemented!()
-    }
-
-    unsafe fn get_object_from_start_address(_start: Address) -> ObjectReference {
-        unimplemented!()
-    }
-
-    fn get_object_end_address(_object: ObjectReference) -> Address {
-        unimplemented!()
-    }
-
     fn get_type_descriptor(_reference: ObjectReference) -> &'static [i8] {
         unimplemented!()
-    }
-
-    fn is_array(_object: ObjectReference) -> bool {
-        unimplemented!()
-    }
-
-    fn is_primitive_array(_object: ObjectReference) -> bool {
-        unimplemented!()
-    }
-
-    fn get_array_length(_object: ObjectReference) -> usize {
-        unimplemented!()
-    }
-
-    fn attempt_available_bits(object: ObjectReference, old: usize, new: usize) -> bool {
-        unsafe {
-            object
-                .to_address()
-                .compare_exchange::<AtomicUsize>(old, new, Ordering::SeqCst, Ordering::SeqCst)
-                .is_ok()
-        }
-    }
-
-    fn prepare_available_bits(object: ObjectReference) -> usize {
-        unsafe { object.to_address().load() }
-    }
-
-    fn write_available_byte(_object: ObjectReference, _val: u8) {
-        unimplemented!()
-    }
-
-    fn read_available_byte(_object: ObjectReference) -> u8 {
-        unimplemented!()
-    }
-
-    fn write_available_bits_word(object: ObjectReference, val: usize) {
-        unsafe {
-            object
-                .to_address()
-                .atomic_store::<AtomicUsize>(val, Ordering::SeqCst)
-        }
-    }
-
-    fn read_available_bits_word(object: ObjectReference) -> usize {
-        unsafe {
-            object
-                .to_address()
-                .atomic_load::<AtomicUsize>(Ordering::SeqCst)
-        }
-    }
-
-    fn gc_header_offset() -> isize {
-        0
     }
 
     fn object_start_ref(object: ObjectReference) -> Address {
@@ -137,25 +60,9 @@ impl ObjectModel<OpenJDK> for VMObjectModel {
         object.to_address()
     }
 
-    fn is_acyclic(_typeref: ObjectReference) -> bool {
-        unimplemented!()
-    }
-
     fn dump_object(object: ObjectReference) {
         unsafe {
             ((*UPCALLS).dump_object)(object);
         }
-    }
-
-    fn get_array_base_offset() -> isize {
-        unimplemented!()
-    }
-
-    fn array_base_offset_trapdoor<T>(_o: T) -> isize {
-        unimplemented!()
-    }
-
-    fn get_array_length_offset() -> isize {
-        unimplemented!()
     }
 }
