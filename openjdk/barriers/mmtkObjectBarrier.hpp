8bd1d0a07e32e70c5e87d6ee21ce432b04c5add2
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-03-11 12:33:59 +1100

    Side-metadata supported object remembering barrier (#50)

--- openjdk/barriers/mmtkObjectBarrier.hpp
+++ openjdk/barriers/mmtkObjectBarrier.hpp
@@ -11,6 +11,13 @@
 #include "../mmtkBarrierSetC1.hpp"
 #include "../mmtkBarrierSetC2.hpp"
 
+#define MMTK_ENABLE_OBJECT_BARRIER_FASTPATH true
+
+#define SIDE_METADATA_BASE_ADDRESS 0x0000060000000000L
+#define SIDE_METADATA_WORST_CASE_RATIO_LOG 1
+#define LOG_BYTES_IN_CHUNK 22
+#define CHUNK_MASK ((1L << LOG_BYTES_IN_CHUNK) - 1)
+
 class MMTkObjectBarrierSetRuntime: public MMTkBarrierSetRuntime {
 public:
   static void record_modified_node_slow(void* src);
@@ -27,7 +34,7 @@ class MMTkObjectBarrierStub;
 
 class MMTkObjectBarrierSetAssembler: public MMTkBarrierSetAssembler {
   void oop_store_at(MacroAssembler* masm, DecoratorSet decorators, BasicType type, Address dst, Register val, Register tmp1, Register tmp2);
-  void record_modified_node(MacroAssembler* masm, Register obj);
+  void record_modified_node(MacroAssembler* masm, Register obj, Register tmp1, Register tmp2);
 public:
   virtual void store_at(MacroAssembler* masm, DecoratorSet decorators, BasicType type, Address dst, Register val, Register tmp1, Register tmp2) {
     if (type == T_OBJECT || type == T_ARRAY) {
@@ -143,31 +150,31 @@ public:
 #define __ ideal.
 
 class MMTkObjectBarrierSetC2: public MMTkBarrierSetC2 {
-  void record_modified_node(GraphKit* kit, Node* node) const;
+  void record_modified_node(GraphKit* kit, Node* node, Node* val) const;
 public:
   virtual Node* store_at_resolved(C2Access& access, C2AccessValue& val) const {
     Node* store = BarrierSetC2::store_at_resolved(access, val);
-    if (access.is_oop()) record_modified_node(access.kit(), access.base());
+    if (access.is_oop()) record_modified_node(access.kit(), access.base(), val.node());
     return store;
   }
   virtual Node* atomic_cmpxchg_val_at_resolved(C2AtomicAccess& access, Node* expected_val, Node* new_val, const Type* value_type) const {
     Node* result = BarrierSetC2::atomic_cmpxchg_val_at_resolved(access, expected_val, new_val, value_type);
-    if (access.is_oop()) record_modified_node(access.kit(), access.base());
+    if (access.is_oop()) record_modified_node(access.kit(), access.base(), new_val);
     return result;
   }
   virtual Node* atomic_cmpxchg_bool_at_resolved(C2AtomicAccess& access, Node* expected_val, Node* new_val, const Type* value_type) const {
     Node* load_store = BarrierSetC2::atomic_cmpxchg_bool_at_resolved(access, expected_val, new_val, value_type);
-    if (access.is_oop()) record_modified_node(access.kit(), access.base());
+    if (access.is_oop()) record_modified_node(access.kit(), access.base(), new_val);
     return load_store;
   }
   virtual Node* atomic_xchg_at_resolved(C2AtomicAccess& access, Node* new_val, const Type* value_type) const {
     Node* result = BarrierSetC2::atomic_xchg_at_resolved(access, new_val, value_type);
-    if (access.is_oop()) record_modified_node(access.kit(), access.base());
+    if (access.is_oop()) record_modified_node(access.kit(), access.base(), new_val);
     return result;
   }
   virtual void clone(GraphKit* kit, Node* src, Node* dst, Node* size, bool is_array) const {
     BarrierSetC2::clone(kit, src, dst, size, is_array);
-    record_modified_node(kit, dst);
+    record_modified_node(kit, dst, NULL);
   }
   virtual bool is_gc_barrier_node(Node* node) const {
     if (node->Opcode() != Op_CallLeaf) return false;
