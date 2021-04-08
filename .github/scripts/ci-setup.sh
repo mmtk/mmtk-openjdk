1686d569651fb888b8c6a17513d4c6ebbfa97428
Yi Lin <qinsoon@gmail.com>
2021-04-08 14:15:09 +1000

    Also install rustfmt

--- .github/scripts/ci-setup.sh
+++ .github/scripts/ci-setup.sh
@@ -4,6 +4,7 @@ set -xe
 rustup toolchain install $RUSTUP_TOOLCHAIN
 rustup target add i686-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
 rustup component add clippy --toolchain $RUSTUP_TOOLCHAIN
+rustup component add rustfmt --toolchain $RUSTUP_TOOLCHAIN
 rustup override set $RUSTUP_TOOLCHAIN
 
 # Download dacapo
