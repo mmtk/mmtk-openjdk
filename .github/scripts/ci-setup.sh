c22c7611d772ab61cba2c9794a7f4b314eaeaebf
Yi Lin <qinsoon@gmail.com>
2021-04-08 13:52:40 +1000

    Install clippy

--- .github/scripts/ci-setup.sh
+++ .github/scripts/ci-setup.sh
@@ -3,6 +3,7 @@ set -xe
 # Install nightly rust
 rustup toolchain install $RUSTUP_TOOLCHAIN
 rustup target add i686-unknown-linux-gnu --toolchain $RUSTUP_TOOLCHAIN
+rustup component add clippy --toolchain $RUSTUP_TOOLCHAIN
 rustup override set $RUSTUP_TOOLCHAIN
 
 # Download dacapo
