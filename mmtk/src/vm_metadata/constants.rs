2214320c087c0d87104c551ed265b9df7ff96a3b
Javad Amiri <javad.amiri@anu.edu.au>
2021-06-27 09:09:23 +1000

    use _VM_BASE_ADDRESS instead of _BASE_ADDRESS

--- mmtk/src/vm_metadata/constants.rs
+++ mmtk/src/vm_metadata/constants.rs
@@ -2,7 +2,7 @@ pub(super) use mmtk::util::constants::LOG_BITS_IN_WORD;
 use mmtk::util::constants::LOG_BYTES_IN_PAGE;
 use mmtk::util::metadata::header_metadata::HeaderMetadataSpec;
 use mmtk::util::metadata::side_metadata::{
-    SideMetadataSpec, GLOBAL_SIDE_METADATA_BASE_ADDRESS, LOCAL_SIDE_METADATA_BASE_ADDRESS,
+    SideMetadataSpec, GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS, LOCAL_SIDE_METADATA_VM_BASE_ADDRESS,
 };
 use mmtk::util::metadata::MetadataSpec;
 
@@ -20,7 +20,7 @@ pub(crate) const FORWARDING_POINTER_OFFSET: usize = 0;
 pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec =
     MetadataSpec::OnSide(SideMetadataSpec {
         is_global: true,
-        offset: GLOBAL_SIDE_METADATA_BASE_ADDRESS.as_usize(),
+        offset: GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize(),
         log_num_of_bits: 0,
         log_min_obj_size: 3,
     });
@@ -57,7 +57,7 @@ pub(crate) const MARKING_METADATA_SPEC: MetadataSpec = MetadataSpec::InHeader(He
 pub(crate) const LOS_METADATA_SPEC: MetadataSpec = MetadataSpec::OnSide(SideMetadataSpec {
     is_global: false,
     offset: if cfg!(target_pointer_width = "64") {
-        LOCAL_SIDE_METADATA_BASE_ADDRESS.as_usize()
+        LOCAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize()
     } else {
         0
     },
