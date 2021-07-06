4b6c4734edc31ffaaeab905b83983fa1971b5513
Yi Lin <qinsoon@gmail.com>
2021-07-06 10:07:28 +1000

    Update mmtk-core

--- mmtk/src/vm_metadata/constants.rs
+++ mmtk/src/vm_metadata/constants.rs
@@ -1,29 +1,25 @@
-pub(super) use mmtk::util::constants::LOG_BITS_IN_WORD;
 use mmtk::util::constants::LOG_BYTES_IN_PAGE;
-use mmtk::util::metadata::header_metadata::HeaderMetadataSpec;
+use mmtk::util::constants::LOG_BYTES_IN_WORD;
 use mmtk::util::metadata::side_metadata::{
-    SideMetadataSpec, GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS, LOCAL_SIDE_METADATA_VM_BASE_ADDRESS,
+    GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS, LOCAL_SIDE_METADATA_VM_BASE_ADDRESS,
 };
-use mmtk::util::metadata::MetadataSpec;
+use mmtk::vm::*;
 
 #[cfg(target_pointer_width = "64")]
-pub(crate) const FORWARDING_BITS_OFFSET: usize = 56;
+pub(crate) const FORWARDING_BITS_OFFSET: isize = 56;
 #[cfg(target_pointer_width = "32")]
-pub(crate) const FORWARDING_BITS_OFFSET: usize = 0;
+pub(crate) const FORWARDING_BITS_OFFSET: isize = 0;
 
-pub(crate) const FORWARDING_POINTER_OFFSET: usize = 0;
+pub(crate) const FORWARDING_POINTER_OFFSET: isize = 0;
 
 // Global MetadataSpecs - Start
 
 /// Global logging bit metadata spec
 /// 1 bit per object
-pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec =
-    MetadataSpec::OnSide(SideMetadataSpec {
-        is_global: true,
-        offset: GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize(),
-        log_num_of_bits: 0,
-        log_min_obj_size: 3,
-    });
+pub(crate) const LOGGING_SIDE_METADATA_SPEC: VMGlobalLogBitSpec = VMGlobalLogBitSpec::side(
+    GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize(),
+    LOG_BYTES_IN_WORD as usize,
+);
 
 // Global MetadataSpecs - End
 
@@ -31,38 +27,28 @@ pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec =
 
 /// PolicySpecific forwarding pointer metadata spec
 /// 1 word per object
-pub(crate) const FORWARDING_POINTER_METADATA_SPEC: MetadataSpec =
-    MetadataSpec::InHeader(HeaderMetadataSpec {
-        bit_offset: FORWARDING_POINTER_OFFSET as isize,
-        num_of_bits: 1 << LOG_BITS_IN_WORD,
-    });
+pub(crate) const FORWARDING_POINTER_METADATA_SPEC: VMLocalForwardingPointerSpec =
+    VMLocalForwardingPointerSpec::in_header(FORWARDING_POINTER_OFFSET);
 
 /// PolicySpecific object forwarding status metadata spec
 /// 2 bits per object
-pub(crate) const FORWARDING_BITS_METADATA_SPEC: MetadataSpec =
-    MetadataSpec::InHeader(HeaderMetadataSpec {
-        bit_offset: FORWARDING_BITS_OFFSET as isize,
-        num_of_bits: 2,
-    });
+pub(crate) const FORWARDING_BITS_METADATA_SPEC: VMLocalForwardingBitsSpec =
+    VMLocalForwardingBitsSpec::in_header(FORWARDING_BITS_OFFSET);
 
 /// PolicySpecific mark bit metadata spec
 /// 1 bit per object
-pub(crate) const MARKING_METADATA_SPEC: MetadataSpec = MetadataSpec::InHeader(HeaderMetadataSpec {
-    bit_offset: FORWARDING_BITS_OFFSET as isize,
-    num_of_bits: 1,
-});
+pub(crate) const MARKING_METADATA_SPEC: VMLocalMarkBitSpec =
+    VMLocalMarkBitSpec::in_header(FORWARDING_BITS_OFFSET);
 
 /// PolicySpecific mark-and-nursery bits metadata spec
 /// 2-bits per object
-pub(crate) const LOS_METADATA_SPEC: MetadataSpec = MetadataSpec::OnSide(SideMetadataSpec {
-    is_global: false,
-    offset: if cfg!(target_pointer_width = "64") {
+pub(crate) const LOS_METADATA_SPEC: VMLocalLOSMarkNurserySpec = VMLocalLOSMarkNurserySpec::side(
+    if cfg!(target_pointer_width = "64") {
         LOCAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize()
     } else {
         0
     },
-    log_num_of_bits: 1,
-    log_min_obj_size: LOG_BYTES_IN_PAGE as usize,
-});
+    LOG_BYTES_IN_PAGE as usize,
+);
 
 // PolicySpecific MetadataSpecs - End
