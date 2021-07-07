a105125853113cd963353e6040eb411d6002bc91
Yi Lin <qinsoon@gmail.com>
2021-07-07 15:34:54 +1000

    Update mmtk-core

--- mmtk/src/vm_metadata/constants.rs
+++ mmtk/src/vm_metadata/constants.rs
@@ -1,5 +1,3 @@
-use mmtk::util::constants::LOG_BYTES_IN_PAGE;
-use mmtk::util::constants::LOG_BYTES_IN_WORD;
 use mmtk::util::metadata::side_metadata::{
     GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS, LOCAL_SIDE_METADATA_VM_BASE_ADDRESS,
 };
@@ -16,10 +14,8 @@ pub(crate) const FORWARDING_POINTER_OFFSET: isize = 0;
 
 /// Global logging bit metadata spec
 /// 1 bit per object
-pub(crate) const LOGGING_SIDE_METADATA_SPEC: VMGlobalLogBitSpec = VMGlobalLogBitSpec::side(
-    GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize(),
-    LOG_BYTES_IN_WORD as usize,
-);
+pub(crate) const LOGGING_SIDE_METADATA_SPEC: VMGlobalLogBitSpec =
+    VMGlobalLogBitSpec::side(GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize());
 
 // Global MetadataSpecs - End
 
@@ -42,13 +38,11 @@ pub(crate) const MARKING_METADATA_SPEC: VMLocalMarkBitSpec =
 
 /// PolicySpecific mark-and-nursery bits metadata spec
 /// 2-bits per object
-pub(crate) const LOS_METADATA_SPEC: VMLocalLOSMarkNurserySpec = VMLocalLOSMarkNurserySpec::side(
-    if cfg!(target_pointer_width = "64") {
+pub(crate) const LOS_METADATA_SPEC: VMLocalLOSMarkNurserySpec =
+    VMLocalLOSMarkNurserySpec::side(if cfg!(target_pointer_width = "64") {
         LOCAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize()
     } else {
         0
-    },
-    LOG_BYTES_IN_PAGE as usize,
-);
+    });
 
 // PolicySpecific MetadataSpecs - End
