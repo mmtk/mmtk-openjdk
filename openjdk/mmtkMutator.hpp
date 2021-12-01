ddf0ba5ce9a69fc1fef3c17135dbe080183407e2
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-12-01 21:52:45 +1100

    Always enable BasePlan spaces (#91)
    * Update los allocators
    
    * Update mmtk-core
    
    * inc MAX_BUMP_ALLOCATORS
    
    * update mmtk
    
    * update mmtk-core
    
    * update mmtk-core
    
    Co-authored-by: Yi Lin <qinsoon@gmail.com>

--- openjdk/mmtkMutator.hpp
+++ openjdk/mmtkMutator.hpp
@@ -19,8 +19,8 @@ struct RustDynPtr {
 };
 
 // These constants should match the constants defind in mmtk::util::alloc::allocators
-const int MAX_BUMP_ALLOCATORS = 5;
-const int MAX_LARGE_OBJECT_ALLOCATORS = 1;
+const int MAX_BUMP_ALLOCATORS = 6;
+const int MAX_LARGE_OBJECT_ALLOCATORS = 2;
 const int MAX_MALLOC_ALLOCATORS = 1;
 const int MAX_IMMIX_ALLOCATORS = 1;
 const int MAX_MARK_COMPACT_ALLOCATORS = 1;
