d4458e48c117dad08609fca855260cb936e060f8
Yi Lin <qinsoon@gmail.com>
2022-02-18 11:34:19 +1100

    Remove use of MMTK_REPO and MMTK_REF

--- .github/scripts/ci-checkout.sh
+++ .github/scripts/ci-checkout.sh
@@ -3,11 +3,6 @@ set -ex
 
 . $(dirname "$0")/common.sh
 
-if [[ ! -z "$MMTK_REPO" && ! -z "$MMTK_VERSION" ]]; then
-    # Replace repo and ref for mmtk-core. @ is used as delimeter for sed.
-    sed -i "s@^mmtk.*git.*rev.*@mmtk = { git = \"https:\/\/github.com\/$MMTK_REPO.git\", rev = \"$MMTK_VERSION\" }@g" $BINDING_PATH/mmtk/Cargo.toml
-fi
-
 OPENJDK_URL=`sed -n 's/^openjdk_repo.=."\(.*\)"$/\1/p' $BINDING_PATH/mmtk/Cargo.toml`
 OPENJDK_VERSION=`sed -n 's/^openjdk_version.=."\(.*\)"$/\1/p' $BINDING_PATH/mmtk/Cargo.toml`
 
