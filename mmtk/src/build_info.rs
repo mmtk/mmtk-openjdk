92d2f32aff246ff58fc0dacb6b09e4459d421f6f
Yi Lin <qinsoon@gmail.com>
2022-08-12 12:09:45 +1000

    Update mmtk-core

--- mmtk/src/build_info.rs
+++ mmtk/src/build_info.rs
@@ -7,8 +7,8 @@ lazy_static! {
     // Owned string for the binding version, such as MMTk OpenJDK 0.14.0 (cfc755f-dirty)
     static ref BINDING_VERSION_STRING: String = match (raw::GIT_COMMIT_HASH, raw::GIT_DIRTY) {
         (Some(hash), Some(dirty)) => format!("MMTk OpenJDK {} ({}{})", raw::PKG_VERSION, hash.split_at(7).0, if dirty { "-dirty" } else { "" }),
-        (None, None) => format!("MMTk OpenJDK {}", raw::PKG_VERSION),
-        x => unreachable!("Unexpected GIT_COMMIT_HASH and GIT_DIRTY {:?}", x)
+        (Some(hash), None) => format!("MMTk OpenJDK {} ({}{})", raw::PKG_VERSION, hash.split_at(7).0, "-?"),
+        _ => format!("MMTk OpenJDK {}", raw::PKG_VERSION),
     };
     // Owned string for both binding and core version.
     static ref MMTK_OPENJDK_FULL_VERSION_STRING: String = format!("{}, using {}", *BINDING_VERSION_STRING, *mmtk::build_info::MMTK_FULL_BUILD_INFO);
