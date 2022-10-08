53ad4e91ceda70c5cb4da747f5731da6aa105da0
Zixian Cai <u5937495@anu.edu.au>
2022-10-08 07:04:43 +0000

    Implement arraycopy_epilogue

--- openjdk/cpu/riscv/mmtkObjectBarrierSetAssembler_riscv.cpp
+++ openjdk/cpu/riscv/mmtkObjectBarrierSetAssembler_riscv.cpp
@@ -8,8 +8,17 @@ void MMTkObjectBarrierSetAssembler::object_reference_write_post(MacroAssembler* 
   assert(false, "Not implemented");
 }
 
-void MMTkObjectBarrierSetAssembler::arraycopy_epilogue(MacroAssembler* masm, DecoratorSet decorators, BasicType type, Register src, Register dst, Register count) {
-  assert(false, "Not implemented");
+void MMTkObjectBarrierSetAssembler::arraycopy_epilogue(MacroAssembler* masm, DecoratorSet decorators, bool is_oop,
+                                  Register src, Register dst, Register count, Register tmp, RegSet saved_regs) {
+  // see also void G1BarrierSetAssembler::gen_write_ref_array_post_barrier
+  assert_different_registers(src, dst, count);
+  const bool dest_uninitialized = (decorators & IS_DEST_UNINITIALIZED) != 0;
+  if (is_oop && !dest_uninitialized) {
+    // in address generate_checkcast_copy, caller tells us to save count
+    __ push_reg(saved_regs, sp);
+    __ call_VM_leaf(FN_ADDR(MMTkBarrierSetRuntime::object_reference_array_copy_post_call), src, dst, count);
+    __ pop_reg(saved_regs, sp);
+  }
 }
 
 #undef __
--- openjdk/cpu/riscv/mmtkObjectBarrierSetAssembler_riscv.hpp
+++ openjdk/cpu/riscv/mmtkObjectBarrierSetAssembler_riscv.hpp
@@ -5,6 +5,7 @@ class MMTkObjectBarrierSetAssembler: public MMTkBarrierSetAssembler {
 protected:
   virtual void object_reference_write_post(MacroAssembler* masm, DecoratorSet decorators, Address dst, Register val, Register tmp1, Register tmp2) const override;
 public:
-  virtual void arraycopy_epilogue(MacroAssembler* masm, DecoratorSet decorators, BasicType type, Register src, Register dst, Register count) override;
+  virtual void arraycopy_epilogue(MacroAssembler* masm, DecoratorSet decorators, bool is_oop,
+                                  Register src, Register dst, Register count, Register tmp, RegSet saved_regs) override;
 };
 #endif // MMTK_OPENJDK_MMTK_OBJECT_BARRIER_SET_ASSEMBLER_RISCV_HPP
