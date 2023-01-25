fa5d5b8cdf7db0711b15fac329ee79768e4984f7
Kunal Sareen <kunal.sareen@anu.edu.au>
2023-01-25 01:42:59 +0000

    Pin python version to python3

--- .github/scripts/ci-checkout.sh
+++ .github/scripts/ci-checkout.sh
@@ -3,8 +3,8 @@ set -ex
 
 . $(dirname "$0")/common.sh
 
-OPENJDK_URL=`cargo read-manifest --manifest-path=$BINDING_PATH/mmtk/Cargo.toml | python -c 'import json,sys; print(json.load(sys.stdin)["metadata"]["openjdk"]["openjdk_repo"])'`
-OPENJDK_VERSION=`cargo read-manifest --manifest-path=$BINDING_PATH/mmtk/Cargo.toml | python -c 'import json,sys; print(json.load(sys.stdin)["metadata"]["openjdk"]["openjdk_version"])'`
+OPENJDK_URL=`cargo read-manifest --manifest-path=$BINDING_PATH/mmtk/Cargo.toml | python3 -c 'import json,sys; print(json.load(sys.stdin)["metadata"]["openjdk"]["openjdk_repo"])'`
+OPENJDK_VERSION=`cargo read-manifest --manifest-path=$BINDING_PATH/mmtk/Cargo.toml | python3 -c 'import json,sys; print(json.load(sys.stdin)["metadata"]["openjdk"]["openjdk_version"])'`
 
 rm -rf $OPENJDK_PATH
 git clone $OPENJDK_URL $OPENJDK_PATH
