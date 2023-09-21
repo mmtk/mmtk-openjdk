2bd206da29716b2c3be33640cd1ba594982275e1
Kunshan Wang <wks1986@gmail.com>
2023-09-21 15:19:36 +0800

    Fix ignored env var options (#244)
    This PR lets MMTk-side Options read environment variables for options
    after OpenJDK-side default options are applied. By doing this,
    environment variables like MMTK_THREADS are no longer ignored.
    
    Fixes: https://github.com/mmtk/mmtk-openjdk/issues/242
    
    This PR makes use of the change introduced in
    https://github.com/mmtk/mmtk-core/pull/955

--- openjdk/mmtk.h
+++ openjdk/mmtk.h
@@ -211,6 +211,7 @@ extern void add_phantom_candidate(void* ref, void* referent);
 extern void mmtk_harness_begin_impl();
 extern void mmtk_harness_end_impl();
 
+extern void mmtk_builder_read_env_var_settings();
 extern void mmtk_builder_set_threads(size_t value);
 extern void mmtk_builder_set_transparent_hugepages(bool value);
 
