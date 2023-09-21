1fae030fb99dab67b83d25e3030c524ba2542e6a
Zixian Cai <2891235+caizixian@users.noreply.github.com>
2023-09-21 16:30:58 +1000

    Update pgo-build.sh

--- .github/scripts/pgo-build.sh
+++ .github/scripts/pgo-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# PGO seems to have problems with incremental compilation or something else similar.
+# PGO build might fail with error messages such as
+#   error: file `/tmp/$USER/pgo-data/merged.profdata` passed to `-C profile-use` does not exist.
+# when the file clearly exists on disk.
+# This happens on both 1.71.1 and 1.66.1, and running cargo clean prior to building seems to reliably work around the problem.
+# We can remove this once the compiler bug is fixed.
 pushd ../mmtk-openjdk/mmtk
 cargo clean
 popd
