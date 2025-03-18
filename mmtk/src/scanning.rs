b6fcbc67fb0a009b6151f6d04e7f35d29d37532e
Kunal Sareen <kunal.sareen@anu.edu.au>
2025-03-18 03:56:08 +0000

    Use buffer size exported from MMTk core

--- mmtk/src/scanning.rs
+++ mmtk/src/scanning.rs
@@ -12,7 +12,7 @@ use mmtk::MutatorContext;
 
 pub struct VMScanning {}
 
-pub(crate) const WORK_PACKET_CAPACITY: usize = 4096;
+pub(crate) const WORK_PACKET_CAPACITY: usize = mmtk::util::constants::BUFFER_SIZE;
 
 extern "C" fn report_slots_and_renew_buffer<S: Slot, F: RootsWorkFactory<S>>(
     ptr: *mut Address,
