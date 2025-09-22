0ebac369eaadd509cbac210a1982872471d6f6c5
Kunshan Wang <wks1986@gmail.com>
2025-09-22 18:33:44 +0800

    Name

--- openjdk/mmtkBarrierSetC1.cpp
+++ openjdk/mmtkBarrierSetC1.cpp
@@ -22,10 +22,10 @@ void MMTkBarrierSetC1::generate_c1_runtime_stubs(BufferBlob* buffer_blob) {
     return Runtime1::generate_blob(buffer_blob, -1, name, false, &closure);
   };
 
-  _load_reference_c1_runtime_code_blob              = do_code_blob("load_reference",              &MMTkBarrierSetAssembler::generate_c1_load_reference_runtime_stub);
-  _object_reference_write_pre_c1_runtime_code_blob  = do_code_blob("object_reference_write_pre",  &MMTkBarrierSetAssembler::generate_c1_object_reference_write_pre_runtime_stub);
-  _object_reference_write_post_c1_runtime_code_blob = do_code_blob("object_reference_write_post", &MMTkBarrierSetAssembler::generate_c1_object_reference_write_post_runtime_stub);
-  _object_reference_write_slow_c1_runtime_code_blob = do_code_blob("object_reference_write_slow", &MMTkBarrierSetAssembler::generate_c1_object_reference_write_slow_runtime_stub);
+  _load_reference_c1_runtime_code_blob              = do_code_blob("c1_load_reference_stub",              &MMTkBarrierSetAssembler::generate_c1_load_reference_runtime_stub);
+  _object_reference_write_pre_c1_runtime_code_blob  = do_code_blob("c1_object_reference_write_pre_stub",  &MMTkBarrierSetAssembler::generate_c1_object_reference_write_pre_runtime_stub);
+  _object_reference_write_post_c1_runtime_code_blob = do_code_blob("c1_object_reference_write_post_stub", &MMTkBarrierSetAssembler::generate_c1_object_reference_write_post_runtime_stub);
+  _object_reference_write_slow_c1_runtime_code_blob = do_code_blob("c1_object_reference_write_slow_stub", &MMTkBarrierSetAssembler::generate_c1_object_reference_write_slow_runtime_stub);
 }
 
 void MMTkC1ReferenceLoadBarrierStub::emit_code(LIR_Assembler* ce) {
