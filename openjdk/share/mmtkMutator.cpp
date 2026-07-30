c93672d203c8dafb024a1c40fa1ab05aaa8aac9e
Yi Lin <qinsoon@gmail.com>
2026-07-30 04:03:46 +0000

    Dont skip post_alloc for default allocation semantics

--- openjdk/share/mmtkMutator.cpp
+++ openjdk/share/mmtkMutator.cpp
@@ -34,7 +34,7 @@ HeapWord* MMTkMutatorContext::alloc(size_t bytes, Allocator allocator) {
   HeapWord* o = (HeapWord*) ::alloc((MMTk_Mutator) this, bytes, HeapWordSize, 0, allocator);
   // Post allocation hooks. Note that we can get a nullptr from mmtk core in the case of OOM.
   // Hence, only call post allocation hooks if we have a proper object.
-  if (o != nullptr && allocator != AllocatorDefault) {
+  if (o != nullptr) {
     ::post_alloc((MMTk_Mutator) this, o, bytes, allocator);
   }
   return o;
