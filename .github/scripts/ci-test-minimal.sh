62ae92428bacf29f556d9d09155658901b77fbe9
Yi Lin <qinsoon@gmail.com>
2026-08-18 04:17:58 +0000

    Add LXR to minimal test

--- .github/scripts/ci-test-minimal.sh
+++ .github/scripts/ci-test-minimal.sh
@@ -16,6 +16,8 @@ MMTK_PLAN=OVC runbms_dacapo2006_with_heap_multiplier fop 4
 MMTK_PLAN=ConcurrentImmix runbms_dacapo2006_with_heap_multiplier fop 4 -XX:-UseCompressedOops -XX:-UseCompressedClassPointers
 MMTK_PLAN=MarkSweep runbms_dacapo2006_with_heap_multiplier fop 8
 MMTK_PLAN=NoGC runbms_dacapo2006_with_heap_size fop 1000 1000
+MMTK_PLAN=LXR MMTK_NO_REFERENCE_TYPES=true runbms_dacapo2006_with_heap_multiplier fop 4
+
 # Test heap resizing
 MMTK_PLAN=GenImmix runbms_dacapo2006_with_heap_size fop 20 100
 # Test compressed oops with heap range > 4GB
